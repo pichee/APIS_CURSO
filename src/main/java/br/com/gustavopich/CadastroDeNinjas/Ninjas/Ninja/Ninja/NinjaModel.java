2c105114b360af7ff546629726a39a7bbd967a4e
Gustavo <76635390+pichee@users.noreply.github.com>
2024-12-08 03:15:18 -0300

    build:arrumando o banco de dados

--- src/main/java/br/com/gustavopich/CadastroDeNinjas/Ninjas/Ninja/Ninja/NinjaModel.java
+++ src/main/java/br/com/gustavopich/CadastroDeNinjas/Ninjas/Ninja/Ninja/NinjaModel.java
@@ -5,8 +5,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
+
 @Entity
 @Table(name = "tb_cadastro_de_ninjas")
 @Data
