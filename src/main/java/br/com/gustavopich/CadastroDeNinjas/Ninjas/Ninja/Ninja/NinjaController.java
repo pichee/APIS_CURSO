cce01bd4f1abb1da9b6ee9e3b34e645e1df4b571
Gustavo <76635390+pichee@users.noreply.github.com>
2024-12-25 11:23:06 -0300

    feat:arrumando todos os caminhos do meu ninja controller

--- src/main/java/br/com/gustavopich/CadastroDeNinjas/Ninjas/Ninja/Ninja/NinjaController.java
+++ src/main/java/br/com/gustavopich/CadastroDeNinjas/Ninjas/Ninja/Ninja/NinjaController.java
@@ -1,14 +1,40 @@
-package br.com.gustavopich.CadastroDeNinjas.Ninjas.Ninja.Ninja;
+  package br.com.gustavopich.CadastroDeNinjas.Ninjas.Ninja.Ninja;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+  @RestController
 @RequestMapping
 public class NinjaController {
-    @GetMapping("/mensagem")
-    public  String boasVindas(){
-        return "MINHA PRIMEIRA MENSAGEM";
-    }
-}
+      @GetMapping("/mensagem")
+      public String boasVindas() {
+          return "MINHA PRIMEIRA MENSAGEM";
+      }
+
+      //ADD ninja
+      @PostMapping("/adicionar")
+      public String criarNinja() {
+          return "Ninja criado";
+      }
+
+      //Read ninja
+      @GetMapping("/todos")
+      public String todos() {
+          return "todos os ninjas";
+      }
+
+      //Read all id
+      @GetMapping("/todosId")
+      public String todosPorId() {
+          return "todos os ninjas por id";
+      }
+
+      @PutMapping("/alterar")
+      public String alterar() {
+          return "alterado";
+      }
+
+      @DeleteMapping("/delete")
+      public String deletar() {
+          return "deletado";
+      }
+  }
---
+++ src/main/resources/db/migrations/V4__Add_rank_TB_CADASTRO_DE_NINJAS.sql
@@ -0,0 +1,3 @@
+--ADD Rank
+ALTER TABLE tb_cadastro_de_ninjas
+ADD COLUMN rank VARCHAR(255);
